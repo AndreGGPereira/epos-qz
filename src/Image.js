2db41d751f41a175e3ca09ba89e0a5fb91b05c3f
Wallyss Lima <wallysslima@gmail.com>
2019-11-26 23:19:12 -0300

    add image example

---
+++ src/Image.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as qz from 'qz-tray';
+import { sha256 } from 'js-sha256';
+
+
+export default class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    qz.api.setSha256Type(data => sha256(data));
+    qz.api.setPromiseType(resolver => new Promise(resolver));
+  }
+
+  componentDidMount() {
+    // Examplo usando React
+    // Por padrão não é possível usar mais de um array para imprimir,
+    // a função recebe apenas um array com todos os dados necessários para
+    // fazer uma impressão.
+    qz.websocket.connect()
+      .then(qz.printers.getDefault)
+      .then(printer => {
+        console.log('impressora default: ', printer)
+        const config = qz.configs.create(printer)
+        const data = [{
+          type: 'image',
+          data: 'logo.svg'
+        }];
+        return qz.print(config, data)
+      })
+      .then(qz.websocket.disconnect)
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return <div>imprimir imagem</div>
+  }
+}
